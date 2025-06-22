eaa535e7fbea7fd7c0b3957e32e6b40cbf789cf6
Dae Dowdell <dow_dell2010@yahoo.com>
2025-06-22 19:53:31 -0400

    Exc 8 complete

--- app.js
+++ app.js
@@ -136,6 +136,21 @@ function evolve(trainedPokemon) {
   }
 
 evolve('Charmander');
+// evolve('Bulbasaur');
 console.log(game.party);
 
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
 
+Solve Exercise 8 here:
+*/
+
+function listMyPokemon() {
+  const names = game.party.map(pokemon => pokemon.name);
+  return names;
+}
+
+const caught = listMyPokemon();
+console.log('My pokemon are', (caught))
